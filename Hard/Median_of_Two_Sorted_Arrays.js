9694ca396285f14aa77540c3a1e97fefced7ba7e
Camron <Cawe219@gmail.com>
2022-12-19 23:18:16 -0600

    Solution for a Medium problem in javascript, C++ and solution for Hard problem.

---
+++ Hard/Median_of_Two_Sorted_Arrays.js
@@ -0,0 +1,28 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    var size = (nums1.length + nums2.length);
+    
+    if(size == 0) return null;
+    if(size == 1) return nums1.length > 0 ? nums1[0] : nums2[0];
+
+    var median = (size + 1)/2
+    var medianMin = Math.floor(median);
+    var medianMax = Math.ceil(median);
+    var result = new Int32Array(medianMax);
+
+    let i=0,j=0;
+
+    
+
+    while(i + j <= medianMax) {
+        if(nums1[i] < nums2[j] || nums2[j] === undefined){
+            result[i+j] = nums1[i++];
+        } else {
+            result[i+j] = nums2[j++];
+        }
+    }
+    
+    //return result;
+    return (result[--medianMin] + result[--medianMax])/2;
+
+
+};
