132c135d9f3a633b0e42e5f6eadd2f9be92feab5
Camron <cawe219@gmail.com>
2023-04-10 19:32:21 -0400

    sighup and login session to app

--- lib/server/app.py
+++ lib/server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, User, Problem, Solutions, UserGraph
-
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data_structures.db'
@@ -14,6 +14,52 @@ migrate = Migrate(app,db)
 db.init_app(app) 
 
 api = Api(app)
+
+
+
+class Signup(Resource):
+    def post(self):
+        username = request.json['username']
+        email = request.json['email']
+        password = request.json['password']
+        user = User(username=username, email=email, password=password)
+
+        user.password_hash = password
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+            print(user.to_dict())
+            
+            return user.to_dict(), 201
+        except IntegrityError:
+            print('IntegrityError')
+            return {'error': '422 Unprocessable Enitity'}, 422
+class CheckSesssion(Resource):
+    def get(self):
+        if 'user_id' in session:
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        else:
+            return {'error': '401 Unauthorized'}, 401
+
+
+class Login(Resource):
+    def post(self):
+        email = request.json['email']
+        password = request.json['password']
+        user = User.query.filter(User.username == username).first()
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            else:
+                return {'error': '401 Unauthorized'}, 401
+
+
+
 
 @app.route('/')
 def index():
@@ -53,3 +99,4 @@ def create_user():
 def get_problem(id):
     problem = Problem.query.filter(Problem.id == id).first()
     return jsonify(problem.to_dict(), 200)
+
