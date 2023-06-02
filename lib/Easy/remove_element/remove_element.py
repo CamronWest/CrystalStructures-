0a964af0071d78ebd13db9967945a0881c703a28
Camron <Cawe219@gmail.com>
2023-06-02 15:05:02 -0500

    remove element python solution

---
+++ lib/Easy/remove_element/remove_element.py
@@ -0,0 +1,9 @@
+def removeElement(nums,val):
+        count = 0 
+        while val in nums:
+            nums.remove(val)  
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count +=1
+        return count
